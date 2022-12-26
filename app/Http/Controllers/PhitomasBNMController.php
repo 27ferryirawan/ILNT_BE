124bcda04ab4220655faf1abc53db73fca243283
Ferry Irawan <27ferryirawan@gmail.com>
2022-12-26 18:57:32 +0700

    BNM done

--- app/Http/Controllers/PhitomasBNMController.php
+++ app/Http/Controllers/PhitomasBNMController.php
@@ -4,59 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 
 class PhitomasBNMController extends Controller
 {
     public function exchangeRates(Request $request){ 
         
-        $tokenClient = new Client(); //site url, username, password
-        $token = $tokenClient->get($request->input('csi_url').'/ido/token/'.$request->input('csi_site').'/'.$request->input('csi_username').'/'.$request->input('csi_password'));
-        $tokenData = json_decode($token->getBody()->getContents(), true)['Token'];
+        $client = new Client(); //site url, username, password
+        $token = $client->request('GET', $request->input('csi_url').'/ido/token/'.$request->input('csi_site').'/'.$request->input('csi_username').'/'.$request->input('csi_password'));
+        $tokenData = json_decode($token->getBody(), true)['Token'];
+        
+        if($tokenData == null || $tokenData == ""){
+            $tokenErrorMessage = json_decode($token->getBody(), true)['Message'];
+            return Response::json(array(
+                'code'      =>  404,
+                'message'   =>  $tokenErrorMessage
+            ), 404);
+        }
+        
         if ($request->input('to_currency') == "" && $request->input('from_currency') != "") {
-            $to_currency = DB::select("
-            select map_curr_code, curr_code
-            from(
-            select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-            where row_id >= (
-            select row_id 
-            from(
-            select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-            where curr_code='" . $request->input('from_currency') . "') order by curr_code asc;");
+            $loadCollectionIDO = 'ILNT_CurrencyMap_mst';
+            $loadCollectionProperties = 'map_curr_code, curr_code';
+            $loadCollectionFilter = "curr_code >= '" .$request->input('from_currency')."'";
         } else if ($request->input('to_currency') != "" && $request->input('from_currency') == "") {
-            $to_currency = DB::select("
-            select map_curr_code, curr_code
-            from(
-            select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-            where row_id <= (
-            select row_id 
-            from(
-            select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-            where curr_code='" . $request->input('to_currency') . "') order by curr_code asc;");
+            $loadCollectionIDO = 'ILNT_CurrencyMap_mst';
+            $loadCollectionProperties = 'map_curr_code, curr_code';
+            $loadCollectionFilter = "curr_code <= '" .$request->input('to_currency')."'";
         } else if ($request->input('to_currency') != "" && $request->input('from_currency') != "") {
-            $to_currency = DB::select("
-            select map_curr_code, curr_code
-            from(
-            select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-            where row_id BETWEEN (
-                select row_id 
-                from(
-                select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-                where curr_code='" . $request->input('from_currency') . "') 
-            AND
-            (
-                select row_id 
-                from(
-                select ROW_NUMBER ( ) OVER ( order by curr_code asc) as row_id, * from ILNT_CurrencyMap_mst ) AS  a 
-                where curr_code='" . $request->input('to_currency') . "')
-                order by curr_code asc;");
+            $loadCollectionIDO = 'ILNT_CurrencyMap_mst';
+            $loadCollectionProperties = 'map_curr_code, curr_code';
+            $loadCollectionFilter = "curr_code BETWEEN '" .$request->input('from_currency')."' AND '".$request->input('to_currency')."'";
         }
 
-        foreach ($to_currency as $data) {
-            $client = new Client(['headers' => ['Accept' => 'application/vnd.BNM.API.v1+json'], 'http_errors' => false]);
+        $validateCheckLotExistsRes = $client->request('GET', $request->input('csi_url') . "/ido/load/" . $loadCollectionIDO . "?properties=" . $loadCollectionProperties . "&filter=" . $loadCollectionFilter, ['headers' => ['Authorization' => $tokenData]]);
+        $to_currency = json_decode($validateCheckLotExistsRes->getBody(), true);
+        
+        if(!$to_currency['Success']){
+            $errorMessage = $to_currency['Message'];
+            return Response::json(array(
+                'code'      =>  404,
+                'message'   =>  $errorMessage
+            ), 404);
+        }
+
+        foreach ($to_currency['Items'] as $data) {
+            $bnmClient = new Client(['headers' => ['Accept' => 'application/vnd.BNM.API.v1+json'], 'http_errors' => false]);
             $quote = 'rm';
-            $res = $client->request('GET', $request->input('bnm_url'). $data->map_curr_code . '/date/' . $request->date . '?session=0900&quote=' . $quote);
-            // return $res->getStatusCode();
+            $res = $bnmClient->request('GET', $request->input('bnm_url'). $data['map_curr_code'] . '/date/' . $request->date . '?session=0900&quote=' . $quote);
             $bnmData = $res->getBody()->getContents();
             $datas = json_decode($bnmData, true);
 
@@ -84,7 +78,7 @@ class PhitomasBNMController extends Controller
                     ],
                     [
                         'Name' => "ToCurrCode",
-                        'Value' => $data->curr_code,
+                        'Value' => $data['curr_code'],
                         'Modified' => true,
                         'ISNull' => false,
                     ],
@@ -109,7 +103,7 @@ class PhitomasBNMController extends Controller
                 ];
             } else {
                 return [
-                    'Currency Code' => $data->curr_code,
+                    'Currency Code' => $data['curr_code'],
                     'Date' => $request->date,
                     'Message' => $datas['message']
                 ];
@@ -124,10 +118,9 @@ class PhitomasBNMController extends Controller
                 'Properties' => $data
             ];
         }
-
+        
         $insertBody['Changes'] = $changes;
-        $insertClient = new Client();
-        $insertRes = $insertClient->request('POST', 'http://20.247.180.239/IDORequestService/ido/update/SLCurrates?refresh=true', ['headers' => ['Authorization' => $tokenData], 'json' => $insertBody]);
+        $insertRes = $client->request('POST', $request->input('csi_url').'/ido/update/SLCurrates?refresh=true', ['headers' => ['Authorization' => $tokenData], 'json' => $insertBody]);
         $insertResponse = json_decode($insertRes->getBody()->getContents(), true);
 
         return response()->json($insertResponse);
